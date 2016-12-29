3ca0e16b1650b668576003a99228059c8d1f3dcc
wsw1125 <454445307@qq.com>
2016-12-29 16:03:49 +0800

    index
    上传

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ window.onload=function(){
 	var divj1=document.getElementById('div1');
 	var btn1=document.getElementsByTagName('input')[0];
 	btn1.onclick=function(){
-		divj1.innerHTML="<input type=\"hidden\"><script defer>alert('hi');<\/script>";
+		alert(divj1.innerHTML);
 	}
 	
 }
